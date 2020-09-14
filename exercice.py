a1b4f550cd77bd1d0c79f4ac8024fd83835018f2
Auzrid <alexandre.cojot@polymtl.ca>
2020-09-14 23:23:28 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -19,7 +19,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    rad1 = angle_degs*2pi
+    rad1 = angle_degs*2*
     return 
 
 
